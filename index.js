1bc46f55e8c7724a88177fbb80c3efff10bd7581
Karemu1842call <victorkinoti03@gmail.com>
2022-06-14 10:41:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const source=["James","Brian","Lebron","Kyrie"]
+function findMatching(source, specification) {
+    return source.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === specification.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(source, specificationName) {
+    return source.filter((record) => record.name === specificationName);
+  }
